23fefadf329412448b1ae4c2cfdac5ac78bd3618
louaimaghrabi <louai.maghrabi@gmail.com>
2019-07-22 19:00:01 +0300

    Task Acheived

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+ 	
+def test(request):
+    context = {
+        "msg": "Hello World!",
+    }
+    return render(request, 'rest.html', context)
